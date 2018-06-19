8a01de64a4e192df3681e91bff6b9695070002c8
caphiphopstudio <40371208+caphiphopstudio@users.noreply.github.com>
2018-06-19 18:54:35 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,5 +6,9 @@ bot.on('message', (message) => {
 	if (message.content == 'capi') {
 		message.channel.sendMessage('PRUEBA');
 	}
+bot.on('message', (message) => {
+	if (message.content == 'capiw') {
+		message.channel.sendMessage('PRUEBAs');
+	}
 });
 bot.login('NDU4MzYyODQyMDM1NTE5NTEx.Dgmj6Q.Gq4ivz9vq_LO1P6KJoalTwBr2C4');
