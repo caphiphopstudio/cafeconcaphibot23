4b963aa133a3e2cbe239eba1178e87d6c72811e9
caphiphopstudio <40371208+caphiphopstudio@users.noreply.github.com>
2018-06-23 16:34:22 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,11 +6,6 @@ bot.on('message', (message) => {
     if (message.content == 'capi22') {
         message.channel.sendMessage('PRUEBA');
     }
-    if (message.content == 'XD') {
-        message.channel.sendMessage('PRUEBAs');
-        bot.channels.get(460076459936055296);
-        message.channel.sendMessage(message.content);
-    }
 });
 
 bot.login(process.env.BOT_TOKEN);
