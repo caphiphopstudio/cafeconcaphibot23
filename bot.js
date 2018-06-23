d5fda1140e6485a92a507a3681791d65105969fd
caphiphopstudio <40371208+caphiphopstudio@users.noreply.github.com>
2018-06-23 16:28:53 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,19 +3,13 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 
 bot.on('message', (message) => {
-	if (message.content == 'capi22') {
-		message.channel.sendMessage('PRUEBA');
-	}
-	if (message.content == 'XD') {
-		message.channel.send('PRUEBAs');
-	}
-	let test = message.guild.channel.find(bot,"test");
-		if test{
-			(message.content == 'XDD');
-			test.send(message.content)
-		}else{console.log("Canal no encontrado)")
-	}
-	
+    if (message.content == 'capi22') {
+        message.channel.sendMessage('PRUEBA');
+    }
+    if (message.content == 'XD') {
+        message.channel.send('PRUEBAs');
+        bot.channels.get(460076459936055296).send(message.content);
+    }
 });
 
 bot.login(process.env.BOT_TOKEN);
