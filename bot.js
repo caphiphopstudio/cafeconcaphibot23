fd99d74e2c92aa779517d39f8e8ff810556dcd1b
caphiphopstudio <40371208+caphiphopstudio@users.noreply.github.com>
2018-06-23 20:31:13 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,8 +6,6 @@ bot.on('message', (message) => {
     if (message.content == 'capi22') {
         message.channel.sendMessage('PRUEBA');
     }
-    var channel = client.servers.get("name", "Café Con Caphi").defaultChannel;
-    client.sendMessage(460076459936055296, "Hello");
 });
 
 bot.login(process.env.BOT_TOKEN);
