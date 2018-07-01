41e1344b53d8fa3d3e32e4c697fa5142dc09c449
caphiphopstudio <40371208+caphiphopstudio@users.noreply.github.com>
2018-07-01 22:36:40 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,6 +6,10 @@ bot.on('message', (message) => {
     if (message.content == 'capi22') {
         message.channel.sendMessage('Mira macho, a ver si te queda claro, me tienes hasta la polla, hasta la mismisima polla, te has , mira, es que , escucha, es bueno, ¡COMEME LA POLLA! ');
     }
+bot.on('message', (message) => {
+    if (message.content == 'capi23') {
+        message.channel.sendMessage('Vale, entiendo, entonces ¿Me comes la polla?');
+    }
 });
 
 bot.login(process.env.BOT_TOKEN);
