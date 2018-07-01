12e37b32c7471cf0ea6ae41be6c5b9acaccb4e65
caphiphopstudio <40371208+caphiphopstudio@users.noreply.github.com>
2018-07-01 22:34:00 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const bot = new Discord.Client();
 
 bot.on('message', (message) => {
     if (message.content == 'capi22') {
-        message.channel.sendMessage('Aceptas las normas del servidor? Pulsa :white_check_mark: para aceptarlas');
+        message.channel.sendMessage('Mira macho, a ver si te queda claro, me tienes hasta la polla, hasta la mismisima polla, te has , mira, es que , escucha, es bueno, ¡COMEME LA POLLA! ');
     }
 });
 
