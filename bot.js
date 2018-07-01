c6d33b5650dd055bd114f388b3855b824c330d1e
caphiphopstudio <40371208+caphiphopstudio@users.noreply.github.com>
2018-07-02 00:23:58 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,43 +10,7 @@ bot.on('message', (message) => {
         message.channel.sendMessage('La ultima vez que abri RR me peto un pulmon de lo subnormales que sois');
     }
     if (message.content == 'capi24') {
-        message.channel.sendMessage('Volando, volando, siempre arriba (Siempre arriba)
-
-imagina, tú y yo, lucharemos los dos.
-
-Volando, volando, siempre arriba (Siempre arriba)
-
-imagina, nunca a un amigo abandonaremos.
-
-Juntos podremos romper un iceberg.
-
-Unamos nuestras manos,
-
-combatamos al mal que nos persigue.
-
-Luz, fuego, destrucción,
-
-el mundo puede ser una ruina,
-
-no lo podemos permitir.
-
-Luz, fuego, destrucción,
-
-a nuestros enemigos hay que vencer,
-
-luchando hasta el final.
-
-Luz, fuego, destrucción,
-
-la paz en el universo ha de nacer,
-
-hemos de hacer un mundo mejor.
-
-Luz, fuego, destrucción,
-
-La fuerza de la verdad nunca morirá,
-
-No morirá, no, no, no, no, no, no, no');
+        message.channel.sendMessage('Volando, volando, siempre arriba (Siempre arriba) imagina, tú y yo, lucharemos los dos. Volando, volando, siempre arriba (Siempre arriba) imagina, nunca a un amigo abandonaremos. Juntos podremos romper un iceberg.  Unamos nuestras manos, combatamos al mal que nos persigue. Luz, fuego, destrucción, el mundo puede ser una ruina, no lo podemos permitir. Luz, fuego, destrucción, a nuestros enemigos hay que vencer, luchando hasta el final. Luz, fuego, destrucción, la paz en el universo ha de nacer, hemos de hacer un mundo mejor. Luz, fuego, destrucción, La fuerza de la verdad nunca morirá, No morirá, no, no, no, no, no, no, no');
     }
 });
 
